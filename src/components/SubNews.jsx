65b6db74728c62aaa4e46ba50ccb3f61334e2083
Arshdeep Singh <arshdeepchhabra1712@gmail.com>
2026-01-31 18:37:31 +0530

    second commit

--- src/components/SubNews.jsx
+++ src/components/SubNews.jsx
@@ -3,15 +3,15 @@ import React, { Component } from 'react'
 
 export class SubNews extends Component {
   render() {
-    let {title,description,imgUrl} = this.props
+    let {title,description,imgUrl,unqId} = this.props
     return (
       <>
       <div className="card" style={{width: "15rem"}}>
   <img src= {imgUrl} className="card-img-top" alt="..."/>
   <div className="card-body">
-    <h5 className="card-title">{title}</h5>
-    <p className="card-text">{description}</p>
-    <a href="/detail" className="btn btn-primary">Read more</a>
+    <h5 className="card-title">{title}...</h5>
+    <p className="card-text">{description}...</p>
+    <a href={unqId} target='_blank' className="btn btn-primary">Read more</a>
   </div>
 </div>
       </>
